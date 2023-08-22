2cb7c31cb0e1cb992873f4834db95d0462082792
Sujoy Kairi <sujoy@esolzmail.com>
2023-08-22 18:25:11 +0530

    Home Page Desktop Version

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+// on hover add class into body
+
+// $(function() {
+//   $('.mega_menu_trigger_wrp').mouseenter(function() {
+//     $('body').addClass("hdr_clr_change");
+//   }).mouseleave(function () {
+//     $('body').removeClass("hdr_clr_change");
+//   });
+// });
+
+// on scroll add class
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+      $("body").addClass("sticky_hdr");
+    } else {
+      $("body").removeClass("sticky_hdr");
+    }
+  });
+});
+
+
+$(function() {
+  
+  var link = $('#navo_bar a.dot');
+  
+  // Move to specific section when click on menu link
+  link.on('click', function(e) {
+    var target = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 200);
+    $(this).addClass('active_one');
+    e.preventDefault();
+  });
+  
+  // Run the scrNav when scroll
+  $(window).on('scroll', function(){
+    scrNav();
+  });
+  
+  // scrNav function 
+  // Change active dot according to the active section in the window
+  function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('main section').each(function() {
+      var id = $(this).attr('id'),
+          offset = $(this).offset().top-1,
+          height = $(this).height();
+      if(sTop >= offset && sTop < offset + height) {
+        link.removeClass('active_one');
+        $('#navo_bar').find('[data-scroll="' + id + '"]').addClass('active_one');
+      }
+    });
+  }
+  scrNav();
+});
